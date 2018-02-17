e27957ea3319f9dd8a6280ef7303307bd9ee9896
JosephRivera10 <joseph.rivera.09@gmail.com>
2018-02-17 01:01:52 -0600

    liri

--- liri.js
+++ liri.js
@@ -48,25 +48,30 @@ function liriCall(action, userEntry) {
 }
 
 
-
 function myTweets () {
 
-var client = new Twitter (keys.twitter);
-var params = {
+	var client = new Twitter (keys.twitter);
+	var params = {
  	screen_name: "Joseph_Rivera_9"
  	// count: 20
- };
-client.get("statuses/user_timeline", params, function(error, tweets, response) {
+ 	};
+	client.get("statuses/user_timeline", params, function(error, tweets, response) {
  	if (!error && response.statusCode === 200) {
  		console.log("Latest Tweets: ");
 
  		for(i=0; i< tweets.length; i++){
+ 			// if (i >= 0 && i <=20) {
+ 			console.log("=============================================");
  			console.log(tweets[i].text);
  			console.log("Posted on " + tweets[i].created_at);
+ 			console.log("=============================================");
+
+ 			// }
+ 			}
  		}
- 	}
- })
+ 	})
 };
+
 
 function spotifyThisSong(userEntry) {
 
